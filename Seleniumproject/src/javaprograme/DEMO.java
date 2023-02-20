33a156468b7879627950b73a0c31074c619cfe01
raje <sandhyajadhav164@gmail.com>
2023-02-20 17:25:24 +0530

    this is frist commit git

---
+++ Seleniumproject/src/javaprograme/DEMO.java
@@ -0,0 +1,17 @@
+package javaprograme;
+
+public class DEMO {
+	
+	
+	
+	public static void main (String [] args) {
+		
+		int s=10;
+		int b=20;
+		int c=s+b;
+		System.out.println(c);
+		
+		
+	}
+
+}
